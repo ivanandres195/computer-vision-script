32528e6cdffe40a4eef3360a59d30ecfaf5edfd4
ivanandres195 <ivanandres195@gmail.com>
2021-05-20 16:59:52 -0500

    Cambios en la detección de distancias y capacidad de personas.

--- detector.py
+++ detector.py
@@ -41,6 +41,8 @@ outs = net.forward(output_layers)
 class_ids = []
 confidences = []
 boxes = []
+centros = [] #Apoyo para medir las distancias entre personas
+
 for output in outs:
     for detection in output:
         #Detecting confidence in 3 steps
@@ -61,20 +63,75 @@ for output in outs:
             boxes.append([x, y, w, h])
             confidences.append(float(confidence))
             class_ids.append(class_id)
-            # cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            #cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            centros.append([center_x, center_y, x, y, w, h]) #Apoyo para medir las distancias entre personas
+            
 
 #Removing Double Boxes
-indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
+indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4) #0.3, 0.4
+w_prom = 0
+w_contador = 0
+centros_f = []
+n_personas = 0 #Contador de personas
 
 for i in range(len(boxes)):
-    if i in indexes[0]:
+    if i in indexes: #original if i in indexes[0]:
         x, y, w, h = boxes[i]
         label = classes[class_ids[i]]  # name of the objects
        
         cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv.putText(img, label, (x, y), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+        cv.putText(img, label, (x, y), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2) #indica que es una persona
 
-title = f"Found <persons={indexes[0]}>"
+        centros_f.append(centros[i])
+        w_prom = w_prom + w #Se obtiene el promedio del tamaño de las cajas
+        w_contador += 1
+        n_personas += 1 #Contador de personas
+
+if n_personas>0:
+    w_prom = w_prom/w_contador #Obtiene el promedio del ancho de las cajas para usarlo como medida de distancia relativa
+
+#Determina si la persona está a una distancia segura de la otra
+#Ubica los centros de las personas y mide sus distancias según w_prom
+distancia = ""
+validador = 0
+color = (0,0,0)
+for i in range(0,n_personas):
+    centroi = centros_f[i]
+    Validador = 0
+    distancia="Distancia segura"
+    color = (0,255,0)
+    for centroj in centros_f:
+        centro_x, centro_y, x, y, w, h = centroi
+        centroA_x, centroA_y, x2, y2, w2, h2 = centroj
+        if centroi != centroj:
+            if abs(centro_x-centroA_x) > w_prom*1.7 or abs(centro_y-centroA_y) > w_prom*1.7: #Determina la distancia segura promedio de la hitbox por 1,7 hitboxes en distancia
+                if validador == 0:
+                    distancia = "Distancia segura"
+                    color = (0,255,0)
+            else:
+                validador = 1
+                distancia = "Distancia no segura"
+                color = (0,0,255)
+    if n_personas == 1:
+        distancia = "Distancia segura"
+        color = (0,255,0)
+    cv.putText(img, distancia, (x, y-10), cv.FONT_HERSHEY_PLAIN, 1, color, 2)
+
+
+#Escribe si el área está sobrepopulada según la cantidad de personas en la imagen
+#El límite de personas puede ser adecuado según el lugar
+if n_personas <= 25: #25
+    seguridad = "Nivel de capacidad: Area libre"
+    cv.putText(img, seguridad, (0, 10), cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+if n_personas > 25 and n_personas <= 50: #25 a 50
+    seguridad = "Nivel de capacidad: Area congestionada, tome precauciones"
+    cv.putText(img, seguridad, (0, 10), cv.FONT_HERSHEY_PLAIN, 1, (100, 255, 255), 2)
+if n_personas > 50: # 50
+    seguridad = "Nivel de capacidad: Exceso de personas"
+    cv.putText(img, seguridad, (0, 10), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+
+title = f"Found <persons={n_personas}>"
 cv.imshow(title, img)
 cv.waitKey(0)
 cv.destroyAllWindows()
